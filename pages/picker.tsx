13b0dd4dfe69b2f1562d717a17c17b278ac9969f
azerifox <azerifox.dev@outlook.com>
2022-10-11 21:24:51 +0200

    Adds reset and random run count

--- pages/picker.tsx
+++ pages/picker.tsx
@@ -23,10 +23,11 @@ function Roulette(props: RouletteProps) {
   let timerId: number | null = null;
   const numberOfParticipants = props.participants.length;
 
-  const [selected, setSelected] = useState<string>();
+  const [selected, setSelected] = useState<string>("?");
   const [rouletteQueue, setRouletteQueue] = useState<Array<string>>(["?"]);
   const [currentQueueIndex, setCurrentQueueIndex] = useState(0);
   const [picking, setPicking] = useState<boolean>(false);
+  const [hasResult, setHasResult] = useState<boolean>(false);
 
   useEffect(() => {
     setRouletteQueue(shuffle(props.participants));
@@ -39,6 +40,13 @@ function Roulette(props: RouletteProps) {
   const pick = () => {
     setPicking(true);
 
+    const runCount = getRandomInt(
+      numberOfParticipants * 3,
+      numberOfParticipants * 4
+    );
+
+    console.log(`runCount: ${runCount}`);
+
     timerId = window.setInterval(() => {
       counter++;
 
@@ -46,19 +54,27 @@ function Roulette(props: RouletteProps) {
         previousIndex === numberOfParticipants - 1 ? 0 : previousIndex + 1
       );
 
-      if (counter === 15) {
+      if (counter === runCount) {
         window.clearInterval(timerId as number);
+        counter = 0;
         setPicking(false);
+        setHasResult(true);
       }
     }, 100);
   };
+
+  const reset = () => {
+    setCurrentQueueIndex(0);
+    setHasResult(false);
+    setSelected("?");
+  }
 
   const listItems = rouletteQueue.map((participant, index) => {
     return (
       <li key={participant}>
         <div className={styles.row}>
           <div className={styles.cursor}>
-            {currentQueueIndex === index && selected != undefined ? ">" : ""}
+            {currentQueueIndex === index && (picking || hasResult) ? ">" : ""}
           </div>
           <div className={styles.column}>{participant}</div>
         </div>
@@ -66,14 +82,20 @@ function Roulette(props: RouletteProps) {
     );
   });
 
+  const button = hasResult ? (
+    <button onClick={reset}>Reset</button>
+  ) : (
+    <button disabled={picking} onClick={pick}>
+      {picking ? "Picking" : "Pick"}
+    </button>
+  );
+
   return (
     <div>
       <h1>Roulette</h1>
-      <p>{selected}</p>
+      <p>{picking || hasResult ? selected : "?"}</p>
       <ul className={styles.nakedList}>{listItems}</ul>
-      <button disabled={picking} onClick={pick}>
-        {picking ? "Picking" : "Pick"}
-      </button>
+      {button}
     </div>
   );
 }
@@ -98,6 +120,13 @@ function shuffle(input: string[]) {
   }
 
   return elements;
+}
+
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  // minimum inclusive, maximum exclusive
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 export default PickerPage;
