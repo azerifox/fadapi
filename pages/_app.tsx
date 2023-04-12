795ad32dd2927c4f7263ab3e48cefd3a40857aa2
azerifox <azerifox.dev@outlook.com>
2023-04-12 13:34:47 +0200

    Adds next-auth with Github provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../app/globals.css'
-import type { AppProps } from 'next/app'
+import "../app/globals.css";
+import { Session } from "next-auth";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session }>) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,42 +2,59 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import styles from "../app/Home.module.css";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Home: NextPage = () => {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <div className={styles.container}>
+        <Head>
+          <title>Fadapi</title>
+          <meta name="description" content="fair daily picker" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main className={styles.main}>
+          <h1 className={styles.title}>fadapi - fair daily picker ;)</h1>
+
+          <div className={styles.grid}>
+            <Link href="/picker">
+              <a className={[styles.card, styles.longCard].join(" ")}>
+                <h2>Held des Tages</h2>
+                <p>
+                  Finden wir den Master of Desaster, den Held im Feld, El Gran
+                  Jefe, die Maus im Haus, den Storyschließenden
+                  Peitschenschwinger...
+                </p>
+              </a>
+            </Link>
+            <Link href="/confetti">
+              <a className={[styles.card, styles.longCard].join(" ")}>
+                <h2>Bedarfskonfetti</h2>
+                <p>
+                  Virtuelles Konfetti vom Discounter, für Situationen wie "Zünd'
+                  den Knaller" und Erfolge jeder Art.
+                </p>
+              </a>
+            </Link>
+          </div>
+
+          <div>
+            Angemeldeter Benutzer: {session.user?.name} <br />
+            <button onClick={() => signOut()}>Sign out</button>
+          </div>
+        </main>
+      </div>
+    );
+  }
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Fadapi</title>
-        <meta name="description" content="fair daily picker" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>fadapi - fair daily picker ;)</h1>
-
-        <div className={styles.grid}>
-          <Link href="/picker">
-            <a className={[styles.card, styles.longCard].join(" ")}>
-              <h2>Held des Tages</h2>
-              <p>
-                Finden wir den Master of Desaster, den Held im Feld, El Gran
-                Jefe, die Maus im Haus, den Storyschließenden
-                Peitschenschwinger...
-              </p>
-            </a>
-          </Link>
-          <Link href="/confetti">
-            <a className={[styles.card, styles.longCard].join(" ")}>
-              <h2>Bedarfskonfetti</h2>
-              <p>
-                Virtuelles Konfetti vom Discounter, für Situationen wie "Zünd'
-                den Knaller" und Erfolge jeder Art.
-              </p>
-            </a>
-          </Link>
-        </div>
-      </main>
-    </div>
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
   );
 };
 
